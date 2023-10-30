e17dba6e7354df7926b5be34de979cb4bfd2507f
Frédéric Thierrée <fredericthierree@gmail.com>
2023-10-30 15:33:23 +0100

    Basic frontend to upload image and log image classification prediction in console

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,25 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import {
+  Container,
+  CssBaseline,
+  Grid,
+  IconButton,
+  PaletteMode,
+  Stack,
+} from "@mui/material";
+import { Cookies } from "react-cookie";
+
+// import Dropzone from "./components/Dropzone";
+
+import { Brightness4, Brightness7 } from "@mui/icons-material";
+import FileUpload from "./components/FileUpload";
 
 function App() {
+  const cookies = new Cookies();
+  const [themeMode, setThemeMode] = useState<PaletteMode>(
+    cookies.get("theme") || "dark"
+  );
+
+  const theme = createTheme({
+    palette: {
+      mode: themeMode,
+      primary: {
+        main: "#d0d0d0",
+        light: "#ac6000",
+        dark: "#ffbe4c",
+        contrastText: "#000000",
+      },
+      secondary: {
+        main: "#5a68e5",
+        light: "#9196ff",
+        dark: "#0d3eb2",
+        contrastText: "#000000",
+      },
+    },
+    components: {
+      MuiCssBaseline: {
+        styleOverrides: {
+          body: {
+            background:
+              themeMode === "light"
+                ? "linear-gradient(180deg, #1a1a1a 0%, #2d2d2d 100%)"
+                : "linear-gradient(90deg, #f7f7f7 0%, #dcdcdc 100%)",
+          },
+        },
+      },
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider theme={theme}>
+      <CssBaseline enableColorScheme>
+        <IconButton
+          color="inherit"
+          sx={{ position: "fixed", right: 25, top: 25, zIndex: 5 }}
+          onClick={() => {
+            let newTheme: PaletteMode = themeMode === "dark" ? "light" : "dark";
+            setThemeMode(newTheme);
+            cookies.set("theme", newTheme);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {cookies.get("theme") === "dark" ? <Brightness7 /> : <Brightness4 />}
+        </IconButton>
+        <Container
+          style={{
+            width: "100vh",
+            height: "100vh",
+          }}
+        >
+          <Grid
+            container
+            justifyContent="center"
+            alignItems="center"
+            style={{ height: "100vh" }}
+          >
+            <Grid item>
+              <Stack spacing={2}>
+                {/* <Dropzone /> */}
+                <FileUpload />
+              </Stack>
+            </Grid>
+          </Grid>
+        </Container>
+      </CssBaseline>
+    </ThemeProvider>
   );
 }
 
---
+++ frontend/app/src/components/FileUpload.tsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+
+import { Button } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import { DropzoneArea } from "react-mui-dropzone";
+import { FormProvider, useForm } from "react-hook-form";
+
+import { apiFetch } from "../utils/Fetch";
+
+const useStyles = makeStyles(() => ({
+  dropzoneContainer: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    minHeight: "200px", // Set a minimum height to ensure a centered image preview
+    border: "2px dashed #cccccc",
+    borderRadius: "4px",
+    padding: "20px",
+    backgroundColor: "#f8f8f8",
+    color: "#333",
+    outline: "none",
+    transition: "border .24s ease-in-out",
+    cursor: "pointer",
+  },
+}));
+
+type FormData = {
+  file: FileList | null;
+};
+
+export const FileUpload = (): JSX.Element => {
+  const classes = useStyles();
+  const methods = useForm<FormData>();
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+  const {
+    handleSubmit,
+    formState: { isDirty },
+  } = methods;
+
+  const onSubmit = () => {
+    if (selectedFile) {
+      const formDataToSend = new FormData();
+      formDataToSend.append("file", selectedFile);
+
+      apiFetch("/api/image_classification/classify", "POST", {}, formDataToSend)
+        .then((response) => console.log(response))
+        .catch((error) => console.error("Error:", error));
+    } else {
+      console.error("No file selected.");
+    }
+  };
+
+  const handleFileChange = (files: File[]) => {
+    if (files.length > 0) {
+      setSelectedFile(files[0]);
+    } else {
+      setSelectedFile(null);
+    }
+  };
+
+  return (
+    <>
+      <FormProvider {...methods}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <DropzoneArea
+            acceptedFiles={["image/*"]}
+            dropzoneText={
+              "Drag and drop an image to perform image classification"
+            }
+            filesLimit={3}
+            showAlerts={isDirty}
+            previewGridClasses={{
+              container: classes.dropzoneContainer,
+            }}
+            onChange={handleFileChange}
+          />
+          <Button type="submit">Upload File</Button>
+        </form>
+      </FormProvider>
+    </>
+  );
+};
+
+export default FileUpload;
